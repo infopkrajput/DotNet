445fe97bed762ba62d046f16e2a8cbfbe562e44e
infopkrajput <p.k.rajput@hotmai.com>
2026-01-21 13:30:28 +0530

    update

---
+++ CompleteVB.NET/Windows/WinFormAppStart/WinFormAppStart/Form1.Designer.vb
@@ -0,0 +1,66 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class MainForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        Button1 = New Button()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(324, 69)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(112, 15)
+        Label1.TabIndex = 0
+        Label1.Text = "First Windows Form"
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(340, 197)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(75, 23)
+        Button1.TabIndex = 1
+        Button1.Text = "Button1"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' MainForm
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(774, 440)
+        Controls.Add(Button1)
+        Controls.Add(Label1)
+        Name = "MainForm"
+        Text = "My First Form"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+    Friend WithEvents EditToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents FormatToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button1 As Button
+
+End Class
+
---
+++ CompleteVB.NET/Windows/WinFormAppStart/WinFormAppStart/Form1.vb
@@ -0,0 +1,8 @@
+﻿Public Class MainForm
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+End Class
